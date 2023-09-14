cb49ded80ce704953ed9e4a641db8d2be8bac612
vishal9151 <84173784+vishal9151@users.noreply.github.com>
2023-09-14 13:11:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,51 @@
-const carousel = document.querySelector(".carousel-container");
-  const leftArrow = document.querySelector(".left-arrow");
-  const rightArrow = document.querySelector(".right-arrow");
-  const images = document.querySelectorAll(".carousel-image");
-  const totalImages = images.length;
-  const imageWidth = images[0].getBoundingClientRect().width;
-
-
-// Function to move to the next slide
+const carousel = document.querySelector(".carousel");
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
 
 let currentIndex = 0;
 let prevIndex;
+const images = document.querySelectorAll(".carousel-image");
 
-// Function to move to the next slide
-function nextSlide() {
-  console.log("in next slide");
-  prevIndex = currentIndex;
-  currentIndex = (currentIndex + 1) % totalImages;
-  updateCarousel();
-}
+const totalImages = Object.keys(images).length;
 
-// Function to move to the previous slide
-function prevSlide() {
+
+const imageWidth = images[0].getBoundingClientRect().width + 40;
+// const imageWidth = 450;
+console.log("getbounding1", images[1].getBoundingClientRect());
+
+leftArrow.addEventListener("click", () => {
   prevIndex = currentIndex;
   currentIndex = (currentIndex - 1 + totalImages) % totalImages;
-  updateCarousel();
-}
+  carousel.style.transform = `translateX(-${imageWidth}px)`;
+  carousel.insertBefore(images[currentIndex], carousel.firstChild);
 
-// Function to update the carousel display
-function updateCarousel() {
-  carousel.style.transform = `translateX(-${currentIndex * imageWidth}px)`;
-}
+  setTimeout(() => {
+    carousel.style.transform = "";
+    carousel.classList.add("sliding-transition");
+  }, 10);
 
-// Automatically change slides every 2 seconds
-setInterval(nextSlide, 2000);
+  setTimeout(() => {
+    carousel.classList.remove("sliding-transition");
+  }, 490);
+});
 
-// Add event listeners for left and right arrow buttons
-leftArrow.addEventListener("click", prevSlide);
-rightArrow.addEventListener("click", nextSlide);
+rightArrow.addEventListener("click", () => {
+  carousel.classList.add("sliding-transition");
 
-// Your original code below
+  prevIndex = currentIndex;
+  currentIndex = (currentIndex + 1) % totalImages;
+
+  carousel.style.transform = `translateX(-${imageWidth}px)`;
+
+  setTimeout(() => {
+    carousel.appendChild(images[prevIndex]);
+    carousel.classList.remove("sliding-transition");
+    carousel.style.transform = "";
+  }, 500);
+});
+
+
+
 const navigators = document.querySelectorAll(".navigators div");
 const carouselNav = document.querySelector(".section1-container");
 
@@ -48,9 +55,11 @@ navigators.forEach((navigator, index) => {
       navigator.classList.remove("active");
     });
     navigator.classList.add("active");
-    carouselNav.style.transform = `translateX(-${window.innerWidth * index}px)`;
+    carouselNav.style.transform = `translateX(-${window.innerWidth* index}px)`;
   });
 });
+
+
 
 const ham = document.querySelectorAll(".ham")[0];
 const icons = document.querySelectorAll(".ham i");
@@ -59,41 +68,45 @@ const body = document.querySelector("body");
 
 ham.addEventListener("click", () => {
   hamMenu.classList.toggle("ham-active");
-
-  if (body.style.overflowY === "hidden") {
+  
+  if(body.style.overflowY === "hidden"){
     body.style.overflowY = "scroll";
-  } else {
+  }else{
     body.style.overflowY = "hidden";
   }
 
   icons.forEach((icon) => {
     icon.classList.toggle("active");
   });
+  
 });
 
+
 var navMenuAnchorTags = document.querySelectorAll('#nav1 p a');
-console.log(navMenuAnchorTags);
 var interval;
 
-for (var i = 0; i < navMenuAnchorTags.length; i++) {
-  navMenuAnchorTags[i].addEventListener('click', function (event) {
-    event.preventDefault();
-    var targetSectionID = this.textContent.trim().toLowerCase();
-    console.log(this.textContent);
-    var targetSection = document.getElementById(targetSectionID);
-    console.log(targetSection);
 
-    interval = setInterval(function () {
-      scrollVertically(targetSection);
-    }, 20);
-  });
+for (var i = 0; i < navMenuAnchorTags.length; i++) {
+    navMenuAnchorTags[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        var targetSectionID = this.textContent.trim().toLowerCase();
+        console.log(this.textContent);
+        var targetSection = document.getElementById(targetSectionID);
+        console.log(targetSection);
+        //    interval = setInterval(scrollVertically, 20, targetSection);
+
+        interval = setInterval(function () {
+            scrollVertically(targetSection);
+        }, 20);
+    });
 }
 
+
 function scrollVertically(targetSection) {
-  var targetSectionCoordinates = targetSection.getBoundingClientRect();
-  if (targetSectionCoordinates.top <= 0) {
-    clearInterval(interval);
-    return;
-  }
-  window.scrollBy(0, 50);
+    var targetSectionCoordinates = targetSection.getBoundingClientRect();
+    if (targetSectionCoordinates.top <= 0) {
+        clearInterval(interval);
+        return;
+    }
+    window.scrollBy(0, 50);
 }
